334979b60edfbecd4fb9b8f19b96157f1d398669
Bulavo72ka <84275986+Bulavo72ka@users.noreply.github.com>
2021-05-16 20:14:10 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1621169781,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/pop_it-sheet0.png",
+		"images/pop_it-sheet1.png",
+		"images/reset-sheet0.png",
+		"images/background.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
